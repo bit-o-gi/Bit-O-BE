e71bf023ecfe75fabc729e250754291e48b52009
ParkSeryu <pjh971212@gmail.com>
2025-01-28 13:22:43 +0900

    hotfix: 유저 기본 조회 API 추가

--- src/main/java/bit/user/entity/UserEntity.java
+++ src/main/java/bit/user/entity/UserEntity.java
@@ -5,17 +5,16 @@ import bit.couple.domain.Couple;
 import bit.user.domain.User;
 import bit.user.enums.OauthPlatformType;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Table(name = "bit_o_user")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
 @Entity
+@Table(name = "bit_o_user")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class UserEntity extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,17 +36,6 @@ public class UserEntity extends BaseEntity {
     @JoinColumn(name = "couple_id")
     private Couple couple;
 
-    @Builder
-    public UserEntity(Long id, String email, String nickName, OauthPlatformType platform, Long providerId, LocalDateTime connectedDt, Couple couple) {
-        this.id = id;
-        this.email = email;
-        this.nickName = nickName;
-        this.platform = platform;
-        this.providerId = providerId;
-        this.connectedDt = connectedDt;
-        this.couple = couple;
-    }
-
     public static UserEntity from(User user) {
         UserEntity userEntity = new UserEntity();
         userEntity.id = user.getId();
@@ -66,6 +54,8 @@ public class UserEntity extends BaseEntity {
                 .email(email)
                 .nickName(nickName)
                 .platform(platform)
+                .providerId(providerId)
+                .connectedDt(connectedDt)
                 .couple(couple)
                 .build();
     }
