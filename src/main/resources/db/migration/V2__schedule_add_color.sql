f0c6eeb223a2b695ccab8ad4065dcb1766fd2b98
parkSeryu <pjh971212@gmail.com>
2025-05-09 08:59:37 +0900

    feat: 스케줄에 컬러 flyway 추가

---
+++ src/main/resources/db/migration/V2__schedule_add_color.sql
@@ -0,0 +1,11 @@
+ALTER TABLE schedule
+    ADD color VARCHAR(255) NULL;
+
+ALTER TABLE day
+    DROP COLUMN test;
+
+ALTER TABLE bit_o_user
+    DROP COLUMN platform;
+
+ALTER TABLE bit_o_user
+    ADD platform VARCHAR(255) NULL;
